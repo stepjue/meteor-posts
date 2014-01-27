5b963a43703b4c4ee35e407e8bdc4dfb3d0705fd
stepjue <stepjue@gmail.com>
2014-01-26 23:41:28 -0800

    added .gitignore

--- server/server.js
+++ server/server.js
@@ -2,17 +2,6 @@ Meteor.publish('posts', function() {
     return Posts.find({}, {sort: {order: 1}});
 });
 
-// facebook account service configuration
-Accounts.loginServiceConfiguration.remove({
-  service: "facebook"
-});
-Accounts.loginServiceConfiguration.insert({
-    service: "facebook",
-    appId: "250849994963069",
-    secret: "a0e5b3d30e772b2b3c78468b478db1d3"
-});
-
-// basically database schemas
 Posts.allow({
   'insert':function(userId, doc) {
     return ((doc.body.length > 5) &&
